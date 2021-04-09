b9f29fa4700609953f5f91746af3648ca8c3d83e
todd-estes <elestes@gmail.com>
2021-04-09 17:35:55 -0600

    Create princess_path_test.rb with happy path test

---
+++ test/princess_path_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require './lib/princess_path.rb'
+
+class PrincessPathTest < Minitest::Test
+  def setup
+    @princess_path = PrincessPath.new
+  end
+
+  def test_it_exists
+    # skip
+    assert_instance_of PrincessPath, @princess_path
+  end
+
+  def test_finds_path
+    n = 3
+    grid = ['--p', '-m-', '---']
+    expected = /UP\nRIGHT\n/
+    assert_output(expected) { @princess_path.display_path_to_princess(n, grid)}
+  end
+end
