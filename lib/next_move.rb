f0d4cc0b5d19af3ab5e605d0bb6afa57b7387b19
todd-estes <elestes@gmail.com>
2021-04-10 14:59:55 -0600

    Add next_move method to NextMove class

--- lib/next_move.rb
+++ lib/next_move.rb
@@ -1,6 +1,21 @@
 class NextMove
+  
+  def self.next_move(n, r, c, grid)
+    princess_position = (find_princess_position(n, grid)).flatten!
+    p_row = princess_position[0]
+    p_col = princess_position[1]
+    if r < p_row
+      puts 'DOWN'
+    elsif r > p_row
+      puts 'UP'
+    elsif c < p_col
+      puts 'RIGHT'
+    elsif c > p_col
+      puts 'LEFT'
+    end
+  end
 
-  def find_princess_position(n, grid)
+  def self.find_princess_position(n, grid)
     row = []
     cell = []
     (0...n).each do |i|
