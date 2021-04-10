239f875a2a7c8cdedc95d8908558f746623bc471
todd-estes <elestes@gmail.com>
2021-04-09 18:01:14 -0600

    Rename variable center to moves for clarity

--- lib/princess_path.rb
+++ lib/princess_path.rb
@@ -1,21 +1,21 @@
 class PrincessPath
 
   def display_path_to_princess(n,grid)
-    center = ((n - 1) / 2)
+    moves = ((n - 1) / 2)
     if grid[0][0] == 'p'
-     center.times do
+     moves.times do
        puts 'UP', 'LEFT'
      end
    elsif grid[0][-1] == 'p'
-     center.times do
+     moves.times do
        puts 'UP', 'RIGHT'
      end
    elsif grid[-1][0] == 'p'
-     center.times do
+     moves.times do
        puts 'DOWN', 'LEFT'
      end
    else
-     center.times do
+     moves.times do
        puts 'DOWN', 'RIGHT'
      end
    end
