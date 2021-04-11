2bc3016220c9f214c99dd5e69577cf0e20abcaa3
todd-estes <elestes@gmail.com>
2021-04-11 14:40:23 -0600

    Add comments and refactor BotSavesPrincess methods for convention

--- lib/bot_saves_princess.rb
+++ lib/bot_saves_princess.rb
@@ -1,31 +1,36 @@
 class BotSavesPrincess
 
   def self.display_path_to_princess(n,grid)
-
+    #moves, i.e. moves traveled from center of grid to princess
     moves = ((n - 1) / 2)
+    #We know princess is in one of four courners
+    #check top-left corner
     if grid[0][0] == 'p'
-     moves.times do
-       puts 'UP', 'LEFT'
-     end
-   elsif grid[0][-1] == 'p'
-     moves.times do
-       puts 'UP', 'RIGHT'
-     end
-   elsif grid[-1][0] == 'p'
-     moves.times do
-       puts 'DOWN', 'LEFT'
-     end
-   else
-     moves.times do
-       puts 'DOWN', 'RIGHT'
-     end
-   end
+      moves.times do
+        puts 'UP', 'LEFT'
+      end
+     #check top-right corner
+    elsif grid[0][-1] == 'p'
+      moves.times do
+        puts 'UP', 'RIGHT'
+      end
+      #check bottom-left corner
+    elsif grid[-1][0] == 'p'
+      moves.times do
+        puts 'DOWN', 'LEFT'
+      end
+    else
+      #princess must be in bottom-right corner
+      moves.times do
+        puts 'DOWN', 'RIGHT'
+      end
+    end
   end
-end
 
-def validate_size_input(n)
-  #
-  if !n.odd? || n < 3 || n >= 100
-    puts 'RE-ENTER INPUT PARAMETERS'
+  def self.validate_size_input(n)
+    #N must odd and (3 <= N < 100)
+    if !n.odd? || n < 3 || n >= 100
+      puts 'RE-ENTER INPUT PARAMETERS'
+    end
   end
 end
