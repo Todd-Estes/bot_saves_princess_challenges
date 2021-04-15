26b93bfc276d2fa4c9c4506ec49d047ff2cfc562
todd-estes <elestes@gmail.com>
2021-04-15 11:51:28 -0600

    Update navigator_test to test for all outputs

--- test/navigator_test.rb
+++ test/navigator_test.rb
@@ -32,13 +32,82 @@ class NavigatorTest< Minitest::Test
     assert_equal 1, @matrix.required_moves
   end
 
-  def test_it_can_find_next_move
+  def test_it_can_find_next_move_up
     expected = /UP\n/
     assert_output(expected) { @matrix.next_move }
   end
 
-  def test_it_can_display_direction
+  def test_it_can_find_next_move_right
+    n = 3
+    r = 0
+    c = 0
+    grid = ['m-p', '---', '---']
+    mario = Bot.new(r, c)
+    peach = Princess.new(n, grid)
+    matrix = Navigator.new(n, grid, mario, peach)
+    expected = /RIGHT\n/
+    assert_output(expected) { matrix.next_move }
+  end
+
+  def test_it_can_find_next_move_left
+    n = 3
+    r = 1
+    c = 2
+    grid = ['---', 'p-m', '---']
+    mario = Bot.new(r, c)
+    peach = Princess.new(n, grid)
+    matrix = Navigator.new(n, grid, mario, peach)
+    expected = /LEFT\n/
+    assert_output(expected) { matrix.next_move }
+  end
+
+  def test_it_can_find_next_move_down
+    n = 3
+    r = 1
+    c = 2
+    grid = ['---', '--m', '--p']
+    mario = Bot.new(r, c)
+    peach = Princess.new(n, grid)
+    matrix = Navigator.new(n, grid, mario, peach)
+    expected = /DOWN\n/
+    assert_output(expected) { matrix.next_move }
+  end
+
+  def test_it_can_display_direction_to_top_right_corner
     expected = /UP\nRIGHT\n/
-    assert_output(expected) { @matrix.display_direction}
+    assert_output(expected) { @matrix.display_direction }
+  end
+
+  def test_it_can_display_direction_to_top_left_corner
+    n = 3
+    moves = ((n-1)/2)
+    grid = ['p--', '-m-', '---']
+    mario = Bot.new(moves, moves)
+    peach = Princess.new(n, grid)
+    matrix = Navigator.new(n, grid, mario, peach)
+    expected = /UP\nLEFT\n/
+    assert_output(expected) { matrix.display_direction }
+  end
+
+  def test_it_can_display_direction_to_bottom_left_corner
+    n = 3
+    moves = ((n-1)/2)
+    grid = ['---', '-m-', 'p--']
+    mario = Bot.new(moves, moves)
+    peach = Princess.new(n, grid)
+    matrix = Navigator.new(n, grid, mario, peach)
+    expected = /DOWN\nLEFT\n/
+    assert_output(expected) { matrix.display_direction }
+  end
+
+  def test_it_can_display_direction_to_bottom_right_corner
+    n = 3
+    moves = ((n-1)/2)
+    grid = ['---', '-m-', '--p']
+    mario = Bot.new(moves, moves)
+    peach = Princess.new(n, grid)
+    matrix = Navigator.new(n, grid, mario, peach)
+    expected = /DOWN\nRIGHT\n/
+    assert_output(expected) { matrix.display_direction }
   end
 end
